60d12c10c5780edf7f02b31a28cf61bde2b14e7a
cant_be_bothered <72973155+marrianstjames25@users.noreply.github.com>
2023-05-14 14:08:46 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Hashtables_
+{
+    public class Employee
+    {
+
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public Employee(int employeeID, string FirstName, string LastName)
+        {
+            this.EmployeeID = employeeID;
+            this.LastName = FirstName;
+            this.FirstName = LastName;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"EmployeeId:{this.EmployeeID} \n" +
+                $"First Name: {this.FirstName} \n" +
+                $"Last Name: {this.LastName}");
+        }
+
+
+        ~Employee()
+        {
+            Console.WriteLine("Clean up, close db connection etc");
+        }
+    }
+}
+
